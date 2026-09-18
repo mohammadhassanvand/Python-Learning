cd9564ad1a7917194862429fbf8845502df772dd
mohammadhassanvand <mohammadhassanvand1234@gmail.com>
2026-09-18 21:59:39 +0330

    git commit -m "Initial commit"

---
+++ simple_app.py
@@ -0,0 +1,11 @@
+"""
+Simple Python App
+"""
+
+def main():
+    print("Hello from GitHub!")
+    name = input("Enter your name: ")
+    print(f"Welcome, {name}!")
+
+if __name__ == "__main__":
+    main()
