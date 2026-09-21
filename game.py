11d7183f646275a989604348c7f81ea290396ee7
mohammadhassanvand <mohammadhassanvand1234@gmail.com>
2026-09-21 20:23:17 +0330

    Add CS50 game (guess the number) solution

---
+++ game.py
@@ -0,0 +1,31 @@
+import random
+
+while True:
+    try:
+        level = int(input("Level: "))
+
+        if level > 0:
+            break
+
+    except ValueError:
+        pass
+
+number = random.randint(1, level)
+
+while True:
+    try:
+        guess = int(input("Guess: "))
+
+        if guess <= 0:
+            continue
+
+    except ValueError:
+        continue
+
+    if guess < number:
+        print("Too small")
+    elif guess > number:
+        print("Too large")
+    else:
+        print("Just right!")
+        break
